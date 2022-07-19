7ef969ae606b262c27c9a84019d255a5393a35f7
Rajat Kotian <rajat>
2022-07-19 12:30:19 +0530

    Read me file changes

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ let app = express()
 
 
 
-
-
 app.listen(2040, () => {
     console.log(`server is listening on 2040`);
 });
+
+// mongodb+srv://root:jxVrYmlq9HQo8vR1@cluster0.jcaqg.mongodb.net/?retryWrites=true&w=majority
