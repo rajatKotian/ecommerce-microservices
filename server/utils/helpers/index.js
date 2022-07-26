3b401ff5829b1a7d03f6e8a074b4ef73638f2f50
Rajat Kotian <rajat>
2022-07-27 02:56:35 +0530

    set config helper for the project

--- server/utils/helpers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Helper {
-    static log(values) {
-        return console.log(values);
-    }
-}
-exports.default = Helper;
--- server/utils/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-export default class Helper {
-    static log(values: any) {
-        return console.log(values)
-    }
-}
