3f110ce90d64f91febd70e06e7f795d4425f4065
Rajat Kotian <rajat>
2022-08-23 13:32:59 +0530

    Minor redis port changes

--- server/utils/helpers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Helper = void 0;
-class Helper {
-    static log(values) {
-        return console.log(values);
-    }
-}
-exports.Helper = Helper;
