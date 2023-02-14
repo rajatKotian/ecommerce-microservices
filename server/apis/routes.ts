05d1ac7267861a1c0b9560ff0e6193ca6e57de3f
rkotian12 <rkotian@algobitz.com>
2023-02-14 23:40:45 +0530

    Removed inventory service from code

--- server/apis/routes.ts
+++ server/apis/routes.ts
@@ -1,6 +1,5 @@
 import * as express from 'express'
 import Auth from '../services/Auth'
-import Inventory from '../services/Inventory'
 
 
 
@@ -8,6 +7,5 @@ let router = express.Router();
 
 
 router.use('/v1/auth', Auth)
-router.use('/v1/inventory', Auth)
 
 export default router;
