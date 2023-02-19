de2f73820d3f07a008d236a90ac214ad84ccd609
rkotian12 <rkotian@algobitz.com>
2023-02-19 18:03:44 +0530

    Encryption class for aes

---
+++ server/utils/encryptionHelper/index.ts
@@ -0,0 +1,16 @@
+import { Cipher } from 'crypto';
+import cryptoJS from 'crypto-js';
+import { AppConfig } from '../../config';
+import { IEncryption } from '../interface';
+
+export class AES implements IEncryption {
+    encrypt(value: string): string {
+        const key = AppConfig.get('AESkeys:key')
+        return cryptoJS.AES.encrypt(value, key).toString();
+    }
+    decrypt(value: string): string {
+        const key = AppConfig.get('AESkeys:key')
+        return cryptoJS.AES.decrypt(value, key).toString();
+    }
+
+}
