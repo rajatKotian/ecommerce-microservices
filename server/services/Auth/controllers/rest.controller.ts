344e07a445ed3f11b8f988d5a60c8989261b52a6
rkotian12 <rkotian@algobitz.com>
2023-02-19 18:17:54 +0530

    Encryption class for aes

--- server/services/Auth/controllers/rest.controller.ts
+++ server/services/Auth/controllers/rest.controller.ts
@@ -1,24 +1,27 @@
 import assert from "assert";
 import { validationResult } from "express-validator";
 import { APIService } from "../../../lib"
+import { Encrypt } from "../../../utils/encryptionHelper";
 import { checkErrors } from "../../../utils/helpers";
 import Logger from "../../../utils/helpers/Logger";
+import { IEncryption } from "../../../utils/interface";
 import { AuthService } from "../service";
 
 
 export default class RestController {
     private authService: any;
     private helper: any;
+    private encrypt: IEncryption;
 
     constructor() {
         this.authService = new AuthService();
+        this.encrypt = new Encrypt();
     }
 
     testRoute = async (req: any, res: any) => {
         try {
             res.status(200).send('This route is working well')
         } catch (error) {
-            Logger.error("This is an error log");
             res.status(400).send(error)
         }
     }
@@ -30,7 +33,6 @@ export default class RestController {
             assert.ok(response.success);
             res.status(200).send(response)
         } catch (error) {
-            Logger.error("This is an error log");
             res.status(400).send(error)
         }
     }
@@ -41,7 +43,6 @@ export default class RestController {
             assert.ok(response.success);
             res.status(200).send(response)
         } catch (error) {
-            Logger.error("This is an error log");
             res.status(400).send(error)
         }
     }
--- server/utils/encryptionHelper/index.ts
+++ server/utils/encryptionHelper/index.ts
@@ -3,14 +3,15 @@ import cryptoJS from 'crypto-js';
 import { AppConfig } from '../../config';
 import { IEncryption } from '../interface';
 
-export class AES implements IEncryption {
+export class Encrypt implements IEncryption {
+
     encrypt(value: string): string {
         const key = AppConfig.get('AESkeys:key')
         return cryptoJS.AES.encrypt(value, key).toString();
     }
     decrypt(value: string): string {
         const key = AppConfig.get('AESkeys:key')
-        return cryptoJS.AES.decrypt(value, key).toString();
+        return cryptoJS.AES.decrypt(value, key).toString(cryptoJS.enc.Utf8);
     }
 
 }
