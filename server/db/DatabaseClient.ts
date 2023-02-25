92b8d85c756183d47a620e78325697b95f509aa1
rkotian12 <rkotian@algobitz.com>
2023-02-26 01:02:40 +0530

    Minor changes for strict query

--- server/db/DatabaseClient.ts
+++ server/db/DatabaseClient.ts
@@ -2,13 +2,10 @@ import Mongoose from 'mongoose'
 import { AppConfig } from '../config'
 
 export default class DatabaseClient {
-    constructor() {
-        // this.connect()
-    }
-
     connect(): Promise<any> {
         return new Promise((resolve, reject) => {
             let { uri, options, dockerUri } = AppConfig.get("mongoDB")
+            Mongoose.set('strictQuery', true);
             Mongoose.connect(uri, options, (error) => {
                 if (error) {
                     console.log("Mongodb connection Unsuccessful")
