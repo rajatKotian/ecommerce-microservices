331886bacda0695655ef7ff6209de1ad64074ebf
rajatKotian2698 <rajat@ailoitte.com>
2023-04-25 01:42:51 +0530

    Planned apis for the cart service

--- server/services/Cart/index.ts
+++ server/services/Cart/index.ts
@@ -11,4 +11,27 @@ GRPCServer.startServer()
 let controller = new RestController()
 router.get('/', controller.testRoute)
 
+/*List by all at once Completed, Cancelled, In progress (CACHEABLE)  */
+router.get('/orders', controller.testRoute)
+
+/* List Items in the cart (CACHEABLE) */
+router.get('/cart', controller.testRoute)
+
+/* Add items to the cart  (CACHING TOUCHPOINT) */
+router.put('/cart', controller.testRoute)
+
+/* Delete items from the cart (CACHING TOUCHPOINT) */
+router.delete('/cart', controller.testRoute)
+
+
+
+/* List Items in the Wishlist (CACHEABLE) */
+router.get('/wish-list', controller.testRoute)
+
+/* Add items to the Wishlist (CACHING TOUCHPOINT */
+router.put('/wish-list', controller.testRoute)
+
+/* Delete items from the Wishlist (CACHING TOUCHPOINT */
+router.delete('/wish-list', controller.testRoute)
+
 export default router
