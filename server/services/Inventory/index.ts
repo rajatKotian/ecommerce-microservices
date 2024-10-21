a4f8b7ab11ebb3646ceab25608262b6bf3f5a221
rajatKotian <rajatkotian@yahoo.com>
2024-10-21 22:38:19 +0530

    Minor hcanges

--- server/services/Inventory/index.ts
+++ server/services/Inventory/index.ts
@@ -3,10 +3,9 @@ import * as express from 'express'
 import { checkSchema, validationResult } from 'express-validator';
 import { loginUserSchema, registerUserSchema } from './utils/routeValidation';
 import { RestController } from "./controllers";
-import { GRPCServer } from './utils/servers/GRPCServer';
 
 let router = express.Router();
-GRPCServer.startServer()
+
 let controller = new RestController()
 router.get('/', controller.testRoute)
 
