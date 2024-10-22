b5582372b11f41037751e6d89dd125bcedc63c74
rajatKotian <rajatkotian@yahoo.com>
2024-10-22 21:25:45 +0530

    fix:Limit the scope for the MVP

--- server/services/Cart/router/index.ts
+++ server/services/Cart/router/index.ts
@@ -19,16 +19,4 @@ router.put('/cart', controller.testRoute);
 /* Delete items from the cart (CACHING TOUCHPOINT) */
 router.delete('/cart', controller.testRoute);
 
-
-
-/* List Items in the Wishlist (CACHEABLE) */
-router.get('/wish-list', controller.testRoute);
-
-/* Add items to the Wishlist (CACHING TOUCHPOINT */
-router.put('/wish-list', controller.testRoute);
-
-/* Delete items from the Wishlist (CACHING TOUCHPOINT */
-router.delete('/wish-list', controller.testRoute);
-
-
 export default router;
