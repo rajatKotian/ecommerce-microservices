76d5e9f6425518a8dd8e10d41cc3ef48564dfb01
rajatKotian <rajatkotian@yahoo.com>
2024-10-22 21:09:41 +0530

    chore: Added versioning folder for the APIs

---
+++ server/apis/v1/routes.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import Auth from '../../services/Auth/router';
+import Cart from '../../services/Cart/router';
+
+
+
+let router = express.Router();
+
+
+router.use('/v1/auth', Auth);
+router.use('/v1/cart', Cart);
+
+export default router;
