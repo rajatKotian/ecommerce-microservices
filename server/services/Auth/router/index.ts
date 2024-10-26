276a1ec027dec50042e768283625c053c47dd0c7
rajatKotian <rajatkotian@yahoo.com>
2024-10-26 21:55:35 +0530

    fix: Auth service router changes

--- server/services/Auth/router/index.ts
+++ server/services/Auth/router/index.ts
@@ -5,13 +5,20 @@ import { Services } from '../../../utils/constants';
 import { DatabaseClient } from '../utils/dbClient';
 import { AuthRouter } from './rest.routes';
 
-let router = express.Router();
-const routes = new AuthRouter().initializeRoutes()
 
+// Start the database server connection
 const uri = AppConfig.get("mongoDB:service:auth");
 const dbClient = new DatabaseClient(uri, Services.AUTH);
 dbClient.startDBServer();
 
+/**  
+ * Create a new Express router instance
+ * Initialize routes using the Inventory Router class instance 
+ * Attach the initialized routes to the base path  
+*/
+let router = express.Router();
+let routes = new AuthRouter().initializeRoutes();
 router.use('/', routes);
 
+// Export the configured router as the default export
 export default router;
