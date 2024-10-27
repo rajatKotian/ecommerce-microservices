793a7b20f2760e6ee095245b1d0822e19594f102
rajatKotian <rajatkotian@yahoo.com>
2024-10-27 22:15:18 +0530

    chore: Error handler comments

--- server/utils/responseHandlers/error.helper.ts
+++ server/utils/responseHandlers/error.helper.ts
@@ -1,21 +1,14 @@
-import { ERROR_MESSAGES } from "../constants";
+import { HttpErrorStatusCode } from "../constants";
 
-enum HttpErrorStatusCode {
-    BAD_REQUEST = 400,
-    UNAUTHORIZED = 401,
-    PAYMENT_REQUIRED = 402,
-    FORBIDDEN = 403,
-    NOT_FOUND = 404,
-    INTERNAL_SERVER = 500,
-    NOT_IMPLEMENTED = 501,
-}
-
+// A base error class that extends the built-in JavaScript Error class.
 class BaseError extends Error {
+    // Public properties to store error-related information.
     public readonly success: boolean;
     public readonly httpCode: HttpErrorStatusCode;
     public readonly isOperational: boolean;
     public readonly description: string;
 
+    // Constructor to initialize properties of the error instance.
     constructor(success: boolean, httpCode: HttpErrorStatusCode, isOperational: boolean, description: any) {
         super(description);
         Object.setPrototypeOf(this, new.target.prototype);
@@ -25,12 +18,15 @@ class BaseError extends Error {
         this.isOperational = isOperational;
         this.description = description;
 
+        // Captures the current stack trace, excluding the constructor call from it.
         Error.captureStackTrace(this);
     }
 }
 
+// A specific error class for API-related errors that extends the BaseError class.
 export class APIError extends BaseError {
+    // Constructor to create an APIError instance with default values if not provided.
     constructor(success: boolean, httpCode = HttpErrorStatusCode.INTERNAL_SERVER, isOperational = true, description: any) {
-        super(success, httpCode, isOperational, description)
+        super(success, httpCode, isOperational, description);
     }
 }
